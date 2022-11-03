73a3198c97d6344ef1512b6db234d2209e1977a9
Patrick <pmcl2472@uni.sydney.edu.au>
2022-11-03 21:24:31 +1100

    MACD SO cleanup

--- TradingStrategies/Combination/MACD_Stochastic_Crossover.py
+++ TradingStrategies/Combination/MACD_Stochastic_Crossover.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-@author: vita
-This strategy uses the MACD crossovers and Stochastic crossovers. The stochastic crossover should occur just before the MACD crossover.
-https://www.dailyfx.com/forex/education/trading_tips/daily_trading_lesson/2020/02/11/macd-vs-stochastic.html
-"""
+
 import pandas as pd
 import ta
 import numpy as np
+import os
+import sys
+sys.path.append(os.getcwd())
+from TradingStrategies.IndicatorFunctions.StochasticOscilator import StochasticOscilator
 
 class MACDStochasticCrossover:
     def __init__(self):
@@ -26,29 +25,18 @@ class MACDStochasticCrossover:
         self.df['stoch_line'] = [0] * len(data)
         self.df['stoch_signal'] = [0] * len(data)
     
-    # calculates the macd line
-    def add_macd_line(self):
+    def calculate_MACD(self):
         self.df['macd_line'] = ta.trend.MACD(close=self.close).macd()
-
-    # calculate the macd signal (a 9 period ema of the macd line)
-    def add_macd_signal(self):
         self.df['macd_signal'] = ta.trend.MACD(close=self.close).macd_signal()
 
-    # caluclates stochastic line (%k)
-    def add_stochastic_line(self):
-        stoch = ta.momentum.StochasticOscillator(high=self.high, low=self.low, close=self.close)
-        self.df['stoch_line'] = stoch.stoch()
-
-    # calculate stochastic signal line (%d)
-    def add_stochastic_signal_line(self):
-        stoch_signal = ta.momentum.StochasticOscillator(high=self.high, low=self.low, close=self.close)
-        self.df['stoch_signal'] = stoch_signal.stoch_signal()
+    def calculate_SO(self):
+        self.df["slow_k"], self.df["slow_d"] = StochasticOscilator(self.df, K = 14, D = 3, slowing = 3)   
 
     def determine_signal(self):
         m_line = self.df['macd_line']
         m_signal = self.df['macd_signal']
-        k_line = self.df['stoch_line']
-        d_signal = self.df['stoch_signal']
+        k_line = self.df['slow_k']
+        d_signal = self.df['slow_d']
 
         action = 0
 
@@ -75,13 +63,13 @@ class MACDStochasticCrossover:
         return action
 
     def addIndicatorDf(self):
-        self.indicatorDf = self.df[['time', 'macd_line', 'macd_signal', 'stoch_line', 'stoch_signal']]
+        self.indicatorDf = self.df[['time', 'macd_line', 'macd_signal', 'slow_k', 'slow_d']]
 
     def run(self, data):
         self.addData(data)
         self.add_macd_line()
         self.add_macd_signal()
-        self.add_stochastic_line()
-        self.add_stochastic_signal_line()
+        self.calculate_SO()
+
         self.addIndicatorDf()
         return self.determine_signal(), self.indicatorDf
