a6b69d0907c7daf379583d10159ac9693b014bb1
pmclennan <pmcl2472@uni.sydney.edu.au>
2022-11-12 13:55:09 +1100

    NLH Mapping

--- MappingProcesses/NoLimitHitMapping/NoLimitHitMapping.py
+++ MappingProcesses/NoLimitHitMapping/NoLimitHitMapping.py
@@ -2,11 +2,14 @@ import pandas as pd
 import os
 import datetime
 import sys
+import ta
+pd.set_option('mode.chained_assignment', None)
 
 sys.path.append(os.getcwd())
 from DataFunctions.DataReaders import MT5SymbolsDataReader
 
-def NoLimitHitMapping(data, limit, numBars):
+def NoLimitHitMapping(data, limit, numBars, minPnL = 0.0001):
+
     """
     A function for the "No Limit Hit" mapping process.
     Iterates through the data and labels a trade at each bar if a trade can be made that isn't stopped out within the number of bars.
@@ -15,76 +18,81 @@ def NoLimitHitMapping(data, limit, numBars):
 
     Parameters:
     data (pd.DataFrame): The OHLC dataframe of the data to map.
-    limit (int or float): As specified limit to hold as a stop loss. Nb input in absolute values and not pips.
+    limit (float): A specified limit to hold as a stop loss. Nb input in absolute values and not pips.
+    numBars (int): The specified number of bars to look for the trade. 
+    minPnL (float = 0.0001): A specified minimum PnL level. Will not map trades below this. Set as a proxy 0 by default.
 
     Returns:
     data (pd.DataFrame): The original dataset with extra columns reflecting the mapping.
-    """
+    """    
     
     data['shortLossHit'] = [""] * len(data)
-    data['maxShortPnL'] = [0] * len(data)
+    data['maxShortPnL'] = [""] * len(data)
     data['shortExitIndex'] = [""] * len(data)
 
     data['longLossHit'] = [""] * len(data)
-    data['maxLongPnL'] = [0] * len(data)
+    data['maxLongPnL'] = [""] * len(data)
     data['longExitIndex'] = [""] * len(data)
+
+    data['BestTradePnL'] = [""] * len(data)
 
     data['Signal'] = [0] * len(data)
 
+    for i in range(len(data)-1):
+        openPrice = data.loc[i+1, 'open']        
 
-    for i in range(1, len(data)-numBars-1):
-        openPrice = data.loc[i, 'open']        
-        
         #Long
-        j = data.where(openPrice - data.loc[i+1:numBars+1,'close'] >= limit).first_valid_index()
-        if j is not None and j - i > 1:
-            data.loc[i, 'longLossHit'] = j
+        for j in range(i+2, min(len(data), i+2+numBars)):
+            if openPrice - data.loc[j, 'close'] >= limit:
+                if not data.loc[i+2:j].loc[data.loc[i+2:j, 'close'] - openPrice > minPnL].empty:
+                    data.loc[i, 'longLossHit'] = j
+                    maxPoint = data.loc[data.loc[i+2:j, 'close'].idxmax()]          
+                    data.loc[i,'maxLongPnL'] = maxPoint['close'] - openPrice
+                    data.loc[i,'longExitIndex'] = maxPoint.name                    
+                break
+            if j == min(len(data), i+2+numBars)-1 and not data.loc[i+2:j].loc[data.loc[i+2:j, 'close'] - openPrice > minPnL].empty:
+                maxPoint = data.loc[data.loc[i+2:j, 'close'].idxmax()]          
+                data.loc[i,'maxLongPnL'] = maxPoint['close'] - openPrice
+                data.loc[i,'longExitIndex'] = maxPoint.name
 
-        elif j is None:
-            j = i + numBars + 1
-
-        maxPoint = data.loc[data.loc[i+1:j-1, 'close'].idxmax()]
-
-        if maxPoint['close'] > openPrice:
-            data.loc[i,'maxLongPnL'] = maxPoint['close'] - openPrice
-            data.loc[i,'longExitIndex'] = maxPoint.name
-
+        
         #Short
-        j = data.where(data.loc[i+1:numBars+1,'close'] - openPrice >= limit).first_valid_index()
-        if j is not None and j - i > 1:
-            data.loc[i, 'shortLossHit'] = j
+        for j in range(i+2, min(len(data), i+2+numBars)):
+            if data.loc[j, 'close'] - openPrice >= limit:
+                if not data.loc[i+2:j].loc[openPrice - data.loc[i+2:j, 'close']  > minPnL].empty:
+                    data.loc[i, 'shortLossHit'] = j
+                    minPoint = data.loc[data.loc[i+2:j, 'close'].idxmin()]          
+                    data.loc[i,'maxShortPnL'] = openPrice - minPoint['close']
+                    data.loc[i,'shortExitIndex'] = minPoint.name
+                break
+            if j == min(len(data), i+2+numBars)-1 and not data.loc[i+2:j].loc[openPrice - data.loc[i+2:j, 'close']  > minPnL].empty:
+                minPoint = data.loc[data.loc[i+2:j, 'close'].idxmin()]          
+                data.loc[i,'maxShortPnL'] = openPrice - minPoint['close']
+                data.loc[i,'shortExitIndex'] = minPoint.name
 
-        elif j is None:
-            j = i + numBars + 1
-            
-        minPoint = data.loc[data.loc[i+1:j-1, 'close'].idxmin()]          
-        
-        if minPoint['close'] < openPrice:
-            data.loc[i,'maxShortPnL'] = openPrice - minPoint['close']
-            data.loc[i,'shortExitIndex'] = minPoint.name
-            
-        ##Signal
-        #Case where signal in both directions identified (unlikely)
-        #Map to best trade (long if equal)
-        if (data.loc[i, 'maxLongPnL'] != 0) and (data.loc[i, 'maxShortPnL'] != 0):
-            if (data.loc[i, 'maxLongPnL'] >= data.loc[i, 'maxShortPnL']):
-                data.loc[i, 'Signal'] = 1
-            else:
-                data.loc[i, 'Signal'] = -1
-            
-        #Case where long trade identified
-        elif (data.loc[i, 'maxLongPnL'] != 0) and (data.loc[i, 'maxShortPnL'] == 0):
-            data.loc[i, 'Signal'] = 1
+        if data['maxLongPnL'].iloc[i] != "" and data['maxShortPnL'].iloc[i] == "":
+            data['Signal'].iloc[i] = 1
+            data['BestTradePnL'].iloc[i] = data['maxLongPnL'].iloc[i]
 
-        #Case where short trade identified
-        elif (data.loc[i, 'maxShortPnL'] != 0) and (data.loc[i, 'maxLongPnL'] == 0):
-            data.loc[i, 'Signal'] = -1
+        elif data['maxShortPnL'].iloc[i] != "" and data['maxLongPnL'].iloc[i] == "":
+            data['Signal'].iloc[i] = -1
+            data['BestTradePnL'].iloc[i] = data['maxShortPnL'].iloc[i]
 
+        elif data['maxShortPnL'].iloc[i] != "" and data['maxLongPnL'].iloc[i] != "":
+            
+            if data['maxLongPnL'].iloc[i] >= data['maxShortPnL'].iloc[i]:
+                data['Signal'].iloc[i] = 1
+                data['BestTradePnL'].iloc[i] = data['maxLongPnL'].iloc[i]
+            
+            elif data['maxLongPnL'].iloc[i] < data['maxShortPnL'].iloc[i]:
+                data['Signal'].iloc[i] = -1
+                data['BestTradePnL'].iloc[i] = data['maxShortPnL'].iloc[i]                
+            
         if i % (round(0.01 * len(data), 0)) == 0:
-                print("Progress: {}% ".format(round(100 * (i/len(data)))), end = "\r", flush = True)
-        
-    print("Progess: 100%")
+            print("Mapping Progress: {}%".format(round(i/len(data) * 100)), end = "\r")
 
+    print("Mapping Progress: 100%")
+        
     return data
 
 data_folder = os.path.join(os.getcwd() , 'Datasets', 'OHLC_Only')
@@ -92,13 +100,32 @@ data_filename = "EURUSD.a_M5_201709040000_202109031210.csv"
 data_dir = os.path.join(data_folder, data_filename)
 data = MT5SymbolsDataReader(data_dir)
 
-data = data.iloc
-
 limit = 20/10000
 numBars = 200
 mappedDat = NoLimitHitMapping(data, limit, numBars)
 
-exportDir = os.path.join(os.getcwd(), "MappingProcesses", "NoLimitHitMapping", "MappedDatasets")
+##Add indicators for features - top from backtests; CCI, pSAR, DC (combined with CCI) and Bollinger Bands
+print("Adding CCI")
+mappedDat['CCI'] = ta.trend.CCIIndicator(mappedDat['high'], mappedDat['low'], mappedDat['close'], window = 14).cci()
+
+print("Adding pSAR")
+mappedDat["pSAR"] = ta.trend.PSARIndicator(high = mappedDat['high'], low = mappedDat['low'], close = mappedDat['close'], step = 0.02, max_step = 0.2).psar()
+
+print("Adding DC")
+mappedDat['D_UC'], mappedDat['D_LC'] = [0] * len(mappedDat), [0] * len(mappedDat)
+DC_periods = 20
+for i in range(DC_periods, len(mappedDat)):
+    mappedDat['D_UC'].iloc[i-1] = max(mappedDat['high'].iloc[i-DC_periods:i])
+    mappedDat['D_LC'].iloc[i-1] = min(mappedDat['low'].iloc[i-DC_periods:i])
+
+print("Adding BB")
+mappedDat['BBHigh'] = ta.volatility.BollingerBands(mappedDat['close']).bollinger_hband()
+mappedDat['BBLow'] = ta.volatility.BollingerBands(mappedDat['close']).bollinger_lband()
+
+#Clean NAs from Indicators
+mappedDat = mappedDat[~mappedDat.isna().any(axis = 1)].reset_index(drop = True)
+exportName = data_filename.split(".csv")[0] + "NLH20_200.csv"
+exportDir = os.path.join(os.getcwd(), "MappingProcesses", "NoLimitHitMapping", "MappedDatasets", exportName)
 
 mappedDat.to_csv(exportDir, index = False)
 
